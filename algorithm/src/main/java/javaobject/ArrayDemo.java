cce9a67fbe634d77d149f3105fd17b6f401166c7
warlock.deng <warlock.deng@zhaopin.com.cn>
2019-09-08 19:12:29 +0800

    test

--- algorithm/src/main/java/javaobject/ArrayDemo.java
+++ algorithm/src/main/java/javaobject/ArrayDemo.java
@@ -13,6 +13,32 @@ public class ArrayDemo {
 
         LinkedList<Integer> linkedList = new LinkedList<>();
 
+grow();
+    }
+
+    private static void grow(){
+        // overflow-conscious code
+        int oldCapacity = 31;
+        int newCapacity = oldCapacity + (oldCapacity >> 1);
+
+        String a = String.valueOf(newCapacity);
+    }
+
+
+    public class Array<T> {
+
+        private T[] data;
+
+        private int size;
+
+        public Array() {
+            data = (T[]) new Objects[10];
+        }
+
+        public Array(int capacity) {
+            data = (T[]) new Objects[capacity];
+        }
+
 
 
     }
