d2d186d58752df37f9693995873c331497e6020d
warlock.deng <warlock.deng@zhaopin.com.cn>
2019-09-18 18:29:37 +0800

    test

--- algorithm/src/main/java/tree/RedBlackDemo.java
+++ algorithm/src/main/java/tree/RedBlackDemo.java
@@ -4,7 +4,30 @@ package tree;
  * @author Warlock.deng
  * Created at 2019-09-05
  */
-public class RedBlackDemo {
+public class RedBlackDemo<E> {
+
+    private static final boolean RED = true;
+
+    private static final boolean BLACK = false;
+
+    private class Node {
+
+        private E e;
+
+        private Node left, right;
+
+        private boolean color;
+
+        public Node(E e) {
+            this.e = e;
+            color = BLACK;
+            left = right = null;
+        }
+
+    }
+
+
+
 
 
 
