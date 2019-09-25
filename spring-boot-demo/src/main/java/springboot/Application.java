da38eac2b635a3083143d484419558bf25c0d86c
warlock.deng <warlock.deng@zhaopin.com.cn>
2019-09-25 16:53:59 +0800

    test

--- spring-boot-demo/src/main/java/springboot/Application.java
+++ spring-boot-demo/src/main/java/springboot/Application.java
@@ -2,7 +2,6 @@ package springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Warlock.deng
