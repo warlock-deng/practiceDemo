1d4b3f06311205221fd75175a2ee37380585c63d
warlock.deng <warlock.deng@zhaopin.com.cn>
2021-03-25 20:04:52 +0800

    test

--- spring-boot-demo/src/main/java/springboot/repository/mapper/UserMapper.java
+++ spring-boot-demo/src/main/java/springboot/repository/mapper/UserMapper.java
@@ -1,8 +1,6 @@
 package springboot.repository.mapper;
 
 
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 import springboot.repository.po.User;
 
@@ -15,12 +13,12 @@ import java.util.List;
 public interface UserMapper {
 
     @Select("select * from user")
-    @Results({
-            @Result(property = "id", column = "id"),
-            @Result(property = "name", column = "name"),
-            @Result(property = "age", column = "age"),
-            @Result(property = "email", column = "email")
-    })
+//    @Results({
+//            @Result(property = "id", column = "id"),
+//            @Result(property = "name", column = "name"),
+//            @Result(property = "age", column = "age"),
+//            @Result(property = "email", column = "email")
+//    })
     List<User> userList();
 
 
